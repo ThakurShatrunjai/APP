561936113a0af98a71fb90988921e3be320c228f
Shatrunjai Singh <98958974+ThakurShatrunjai@users.noreply.github.com>
2023-03-06 00:06:50 +0530

    Create Numpy.py

---
+++ Numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+a = np.array([1,2,3])
+print(a)
+
+type(a)
+
+a1 =np.array([(1,2,3),(4,5,6),(7,8,9)])
+print(a1)
+
+print(a.ndim)
+
+print(a1.ndim)
+
+np.linspace(5,50,num=100,endpoint=False,retstep=True)
